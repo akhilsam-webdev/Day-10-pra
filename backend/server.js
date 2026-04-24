33295e60c1600a4ba313749740b84952ffd211e4
Akhil sambasivan <akhilsam7722@gmail.com>
2026-04-24 20:35:28 +0530

    --

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
-require("dotenv").config()
-const app = require("./src/app")
-const connectToDb = require("./src/config/database")
+require("dotenv").config();
+const app = require("./src/app");
+const connectToDb = require("./src/config/database");
 
 // --- ADD THE LOG HERE ---
 console.log("Database URL Key:", "DB_URL"); // Use the name you put in Render
-console.log("Does it exist?", !!process.env.DB_URL); 
+console.log("Does it exist?", !!process.env.DB_URL);
 // ------------------------
 
-connectToDb()
-app.listen(3000,()=>{
-    console.log("server is running on port 3000")
-})
+connectToDb();
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`server is running on port ${port}`);
+});
