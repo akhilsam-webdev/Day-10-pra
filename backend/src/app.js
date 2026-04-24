3262ac0755585294336951048d6f289ff0f98be1
Akhil sambasivan <akhilsam7722@gmail.com>
2026-04-24 20:53:00 +0530

    --

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "../public")));
 
+//
 // 🔹 CREATE
 app.post("/api/note", async (req, res) => {
   const { title, diss } = req.body;
